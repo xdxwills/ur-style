f20b31157f50fc393fad02ffcde74d3e3cb5cba3
xdxwills <144390697+xdxwills@users.noreply.github.com>
2026-07-15 18:37:53 -0400

    Updated animation more

--- ur-style/scenes/tessa_sprite.gd
+++ ur-style/scenes/tessa_sprite.gd
@@ -16,18 +16,20 @@ extends Node3D
 @export var ankle_R: Node3D
 @export var knee_R: Node3D
 
+# UPPER BODY
+@export var upper_body: Node3D
 
 
-var scale_amplitude: float = 0.025
-var arm_rot_amplitude: float = deg_to_rad(2.5)
-var elbow_rot_amplitude: float = deg_to_rad(5)
+var scale_amplitude: float = 0.015
+var arm_rot_amplitude: float = deg_to_rad(1.5)
+var elbow_rot_amplitude: float = deg_to_rad(1.5)
 var ankle_rot_amplitude: float = deg_to_rad(1)
 var knee_rot_amplitude: float = deg_to_rad(3)
 
 # Controls for snappy timing
 var speed_min_factor: float = 0.6    # slowest multiplier (must be > 0)
-var speed_max_factor: float = 2.0    # fastest multiplier
-var speed_snap_pow: float = 3.0      # >1 sharpens peak, <1 flattens it
+var speed_max_factor: float = 1.6    # fastest multiplier
+var speed_snap_pow: float = 0.5      # >1 sharpens peak, <1 flattens it
 
 
 var curr_animation_value: float = 0.0
@@ -68,11 +70,13 @@ func _process(delta: float) -> void:
 
 	ankle_L.rotation.z = solve_cos(curr_rotation, ankle_rot_amplitude, true)
 	knee_L.rotation.z = solve_cos(curr_rotation + PI, knee_rot_amplitude, false)
-	#knee_L.rotation.z = solve_cos(curr_rotation + PI, arm_rot_amplitude, true)
 	
 	ankle_R.rotation.z = solve_cos(curr_rotation + PI, ankle_rot_amplitude, false)
 	knee_R.rotation.z = solve_cos(curr_rotation, knee_rot_amplitude, true)
-	#knee_R.rotation.z = solve_cos(curr_rotation + PI, arm_rot_amplitude, false)
+	
+	
+	upper_body.position.y = solve_cos(curr_rotation, .15, false)
+	
 
 func solve_cos(curr_rotation: float, amplitude: float, direction: bool = true) -> float:
 	var solved_cos: float = cos(curr_rotation) * amplitude * -1.0
